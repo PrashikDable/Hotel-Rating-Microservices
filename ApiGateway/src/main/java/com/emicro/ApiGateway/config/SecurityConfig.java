948530f14e4ebada6b73905efb291780a1777bec
Prashik Dable <prashikdable7@.com>
2025-02-19 17:54:47 +0530

    Added Circuit Breaker, RateLimiter, and OKTA OAuth2.0

---
+++ ApiGateway/src/main/java/com/emicro/ApiGateway/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.emicro.ApiGateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity){
+
+//		httpSecurity
+//				.authorizeExchange()
+//				.anyExchange()
+//				.authenticated()
+//				.and
+//				.oauth2Client()
+//				.and
+//				.oauth2ResourceServer()
+//				.jwt();
+        httpSecurity
+                .authorizeExchange(exchange -> exchange
+                        .anyExchange()
+                        .authenticated()
+                ).oauth2Client(Customizer.withDefaults())
+                .oauth2ResourceServer(oAuth2 -> oAuth2
+                        .jwt(Customizer.withDefaults()));
+
+		return httpSecurity.build();
+	}
+}
--- UserService/UserService/src/main/java/com/micro/userService/controllers/UserController.java
+++ UserService/UserService/src/main/java/com/micro/userService/controllers/UserController.java
@@ -2,6 +2,10 @@ package com.micro.userService.controllers;
 
 import java.util.List;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +19,10 @@ import com.micro.userService.services.UserService;
 public class UserController {
 
 	@Autowired
-	private UserService userService; 
-	
+	private UserService userService;
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
 	@PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User user1 = userService.saveUser(user);
@@ -25,11 +31,39 @@ public class UserController {
 	
 	 //single user get
     @GetMapping("/{userId}")
+    @CircuitBreaker(name= "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelLimiter")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
 
+    // Fallback Method
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception e){
+        logger.info("Fallback is executed because service is down.",e.getMessage());
+
+        User user = User.builder()
+                .email("dummyFallback@gmail.com")
+                .name("dummyFallBack")
+                .about("User is created because this service is down")
+                .userId("234234728734")
+                .build();
+
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+// Rate Limiter
+    public ResponseEntity<User> ratingHotelLimiter(String userId, Exception e){
+        logger.info("Fallback is executed because of multiple request.",e.getMessage());
+
+        User user = User.builder()
+                .email("dummyRateLimiter@gmail.com")
+                .name("dummyRateLimiter")
+                .about("User is created because of multiple request to the API.")
+                .userId("234234728734")
+                .build();
+
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
 
     //all user get
     @GetMapping
